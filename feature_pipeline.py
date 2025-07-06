39d3b903575d379860a2b8639d92639ebe0c3b54
Avazbek <isroilovavazbek2002@gmail.com>
2025-07-06 18:18:01 +0500

    pushing all the data

--- feature_pipeline.py
+++ feature_pipeline.py
@@ -2,6 +2,8 @@ import os
 import requests
 import pandas as pd
 import hopsworks
+import json
+import datetime
 
 project = hopsworks.login(
     api_key_value="wn1dqr4luNONtCPW.y2wmDAXEIgNdRiaqict0iNRgTh12zIoRZmL8olFjSWDmyypo5CwklmbCvVAu43Hm",
@@ -16,31 +18,51 @@ def get_aqi_data(city, api_token):
     if response.status_code == 200:
         data = response.json()['data']
         aqi = data['aqi']
-        time = data['time']['s']
-        return pd.DataFrame([{'city': city, 'time': time, 'aqi': aqi}])
+        date = data['time']['s'].split(' ')[0] # Extract date in 'YYYY-MM-DD' format
+        return pd.DataFrame([{'date': date, 'aqi': aqi}])
     else:
         print("Failed to fetch data")
         return pd.DataFrame()
     
 AQICN_API_TOKEN = "e3c818ba649b427837fea729a38d987d67526f33"
-CITY = "tashkent"
+CITY = "london"
 
 # Fetch the data
 aqi_df = get_aqi_data(CITY, AQICN_API_TOKEN)
 
 if not aqi_df.empty:
     # Basic Feature Engineering: Add a timestamp
-    aqi_df['timestamp'] = pd.to_datetime('now').round('H')
-    
-    # --- Storing Features in Hopsworks ---
-    # Create or get a feature group
-    aqi_fg = fs.get_or_create_feature_group(
+    aqi_df['date'] = pd.to_datetime(aqi_df['date'])
+    aqi_df['aqi'] = aqi_df['aqi'].astype(float)
+
+    aqi_fg = fs.get_feature_group(
         name="aqi_features",
-        version=1,
-        primary_key=["city", "timestamp"],
-        description="Air Quality Index Features"
+        version=1
     )
+
+    now_utc = datetime.datetime.now(datetime.timezone.utc)
+    seven_days_ago = now_utc - datetime.timedelta(days=14)
+
+    query = aqi_fg.select_all()
+
+    query = query.filter(aqi_fg.date > seven_days_ago)
+
+    historical_df = query.read()
+    historical_df["date"] = pd.to_datetime(historical_df["date"]).dt.date
+    aqi_df["date"] = aqi_df["date"].dt.date
+
+    combined_df = pd.concat([historical_df, aqi_df], ignore_index=True)
+    combined_df.sort_values(by='date', inplace=True)
+
+    for i in range(1, 8):
+        combined_df[f'aqi_lag_{i}'] = combined_df['aqi'].shift(i)
     
-    # Insert data into the feature group
-    aqi_fg.insert(aqi_df)
+    combined_df["aqi_rolling_mean_6"] = combined_df['aqi'].shift(1).rolling(window=7).mean()
+    combined_df["aqi_rolling_std_6"] = combined_df['aqi'].shift(1).rolling(window=7).std()
+
+    last_row = combined_df.tail(1).copy()
+    last_row["date"] = pd.to_datetime(pd.to_datetime(last_row['date']).dt.date)
+    last_row.reset_index(drop=True, inplace=True)
+    
+    aqi_fg.insert(last_row)
     print("Successfully inserted new AQI data into the feature store.")
---
+++ training_pipeline.py
@@ -0,0 +1,94 @@
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from hsml.schema import Schema
+from hsml.model_schema import ModelSchema
+import numpy as np
+import xgboost as xgb
+import os
+import requests
+import pandas as pd
+import hopsworks
+import json
+import datetime
+import pickle
+
+project = hopsworks.login(
+    api_key_value="wn1dqr4luNONtCPW.y2wmDAXEIgNdRiaqict0iNRgTh12zIoRZmL8olFjSWDmyypo5CwklmbCvVAu43Hm",
+    project="AQIndex_forecast"
+)
+
+fs = project.get_feature_store()
+
+
+aqi_fg = fs.get_feature_group(
+    name="aqi_features",
+    version=1
+)  
+
+query = aqi_fg.select_all()
+
+print("Fetching all historical data for training...")
+training_df = query.read(online=False)
+print(f"Successfully retrieved {len(training_df)} rows.")
+
+training_df.drop(columns=['date'], inplace=True)
+
+Y = training_df['aqi']
+X = training_df.drop(columns=['aqi'], axis=1)
+
+split_point = int(len(training_df)*0.9)
+
+X_train, X_test = X[:split_point], X[split_point:]
+Y_train, Y_test = Y[:split_point], Y[split_point:]
+
+print(f"Defining the model...")
+model = xgb.XGBRegressor(
+    n_estimators=1000,
+    learning_rate=0.05    # XGBoost handles NaNs by default, no special parameter needed
+)
+
+print("Training the model...")
+model.fit(X_train, Y_train,
+          eval_set=[(X_test, Y_test)],
+          verbose=False)
+
+# 1. Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# 2. Calculate the metrics
+print("Calculating metrics...")
+mae = mean_absolute_error(Y_test, y_pred)
+rmse = np.sqrt(mean_squared_error(Y_test, y_pred))
+r2 = r2_score(Y_test, y_pred)
+
+mr = project.get_model_registry()
+
+metrics = {
+    "mean_absolute_error": mae,
+    "root_mean_squared_error": rmse,
+    "r_squared": r2
+    }
+
+print(f"Mean Absolute Error (MAE): {mae:.2f}")
+print(f"Root Mean Squared Error (RMSE): {rmse:.2f}")
+print(f"R-squared (R2): {r2:.2f}")
+
+filename = 'aqi_xgboost_model.pkl'
+model_dir = "aqi_models"
+
+# 2. Open the file in write-binary ('wb') mode and save the model
+with open(f"{model_dir}/{filename}", 'wb') as file:
+    pickle.dump(model, file)
+
+input_schema = Schema(X_train)
+output_schema = Schema(Y_train)
+model_schema = ModelSchema(input_schema=input_schema, output_schema=output_schema)
+
+# Create a new model object to register
+sklearn_model = mr.sklearn.create_model(
+    name="aqi_forecast_model",
+    metrics=metrics,
+    description="Random Forest model for aqi forecast in London.",
+    model_schema=model_schema,
+)
+
+sklearn_model.save(model_dir)
